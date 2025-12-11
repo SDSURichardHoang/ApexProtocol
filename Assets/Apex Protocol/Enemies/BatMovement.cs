657ae38513ce7337ee4b8253cf607b3e79a1e6a1
grantbecker4 <grantbecker4@gmail.com>
2025-12-11 00:26:26 -0800

    First implementations of movement code
    Didn't get much done today, hoping to get more done Friday

--- Assets/Apex Protocol/Enemies/BatMovement.cs
+++ Assets/Apex Protocol/Enemies/BatMovement.cs
@@ -13,6 +13,32 @@ public class BatMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //used to establish basic position vector
+        Vector3 position = transform.position;
+        float movementValue = Random.value;
+        if (movementValue <= 0.17) //moves in the positive x direction
+        {
+            position.x += Time.deltaTime; //will update with a speed variable later
+        }
+        else if (movementValue > 0.17 && movementValue <= 0.33) //moves in the negative x direction
+        {
+            position.x -= Time.deltaTime; //will update with a speed variable later
+        }
+        else if (movementValue > 0.33 && movementValue <= 0.5) //moves in the positive y direction
+        {
+            position.y += Time.deltaTime; //will update with a speed variable later
+        }
+        else if (movementValue > 0.5 && movementValue <= 0.66) //moves in the negative y direction
+        {
+            position.y -= Time.deltaTime; //will update with a speed variable later
+        }
+        else if (movementValue > 0.66 && movementValue <= 0.83) //moves in the positive z direction
+        {
+            position.z += Time.deltaTime; //will update with a speed variable later
+        }
+        else if (movementValue > 0.83) //moves in the negative z direction
+        {
+            position.z -= Time.deltaTime; //will update with a speed variable later
+        }
     }
 }
