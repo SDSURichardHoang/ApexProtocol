e9e75dcf47fe851cbda3797bc797112796c024fe
rhoang2 <rhoang6809@sdsu.edu>
2025-12-15 21:31:49 -0800

    Game Over Screen created(needs to be implemented with main character) + comments )

--- Assets/Apex Protocol/MenuScripts/GameOver.cs
+++ Assets/Apex Protocol/MenuScripts/GameOver.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
     
+    //Resets level on input of restart button
     public void RestartButtton()
     {
         SceneManager.LoadSceneAsync(1);
     }
 
+    //On hit of main menu button, changes scene to main menu scene
     public void QuitButton()
     {
         SceneManager.LoadSceneAsync(0);
